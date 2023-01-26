4537257d6b3799cbef90d84aacd9707d7f5c4ffd
amit-technogetic <amit.chamnan@technogetic.com>
2023-01-26 16:52:50 +0530

    Added files

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
-
+const connectDB = require('./config/db');
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// connect Database
+connectDB();
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
 });
+// Init middleware
+app.use(express.json({ extended : false}));
+
+
+app.use('/api/users' , require('./routes/api/users'));
+app.use('/api/posts' , require('./routes/api/posts'));
+app.use('/api/profile' , require('./routes/api/profile'));
+app.use('/api/auth' , require('./routes/api/auth'));
 
 app.get('/', (req,res) => {
     res.send("Api Running");
